6e0aa8c8ff0f7d8d4694740b54d04dffcb2dd365
Phoenixxyy14 <phoenixx546@gmail.com>
2025-10-31 12:03:31 +0100

    Updated code and configurations

--- services/blockchain.js
+++ services/blockchain.js
@@ -1,1 +1,8 @@
 // This is where we will put all the functions that interact with the blockchain tomorrow
+
+// function for Phoenixx:
+// | 1 | constructor()  Initializes provider, wallet, and basic config |
+// 2 | getBalance()   Fetch wallet balance (read operation)
+// | 3 | hasEnoughBalance()   Check if faucet has sufficient funds
+//  | 4 | isValidAddress(address) Validate Ethereum address
+// | 5 | estimateGas(toAddress)  Estimate gas cost for a transaction
