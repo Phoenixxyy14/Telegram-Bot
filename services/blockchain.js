c8382b25ecd1765f1abb6c633fde8b135645d9e7
cannotwritecode <openedbakery@gmail.com>
2025-10-31 20:19:58 +0100

    write constructor function

--- services/blockchain.js
+++ services/blockchain.js
@@ -1,1 +1,14 @@
-// This is where we will put all the functions that interact with the blockchain tomorrow
+const { ethers } = require("ethers");
+const config = require("../config/config");
+
+class BlockchainService {
+  constructor() {
+    //Connecting to Sepolia
+    this.provider = new ethers.JsonRpcProvider(config.rpcUrl);
+
+    this.wallet = new ethers.Wallet(config.privateKey, this.provider);
+
+    this.faucetAmount = ethers.parseEther(config.faucetAmount);
+    this.minBalanceAlert = ethers.parseEther(config.minBalanceAlert);
+  }
+}
